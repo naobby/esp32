65aab2ce43de26b33a7e9b97e139af6c5d305cdd
CHOHONGJUN <49662577+CHOHONGJUN@users.noreply.github.com>
2019-04-18 14:30:59 +0900

    Add files via upload

--- ota_updater.py
+++ ota_updater.py
@@ -7,7 +7,7 @@ import ujson
 
 class OTAUpdater:
 
-    def __init__(self, github_repo, module='', main_dir='.'):
+    def __init__(self, github_repo, module='', main_dir='main'):
       self.http_client = HttpClient()
       self.github_repo = github_repo.rstrip('/').replace('https://github.com', 'https://api.github.com/repos')
       self.module = module.rstrip('/')
@@ -331,6 +331,7 @@ class HttpClient:
 
    def delete(self, url, **kw):
      return self.request('DELETE', url, **kw)
+
 
 
 
