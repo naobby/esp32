74940e92b054f32083cdae26efc7b731521ce721
CHOHONGJUN <49662577+CHOHONGJUN@users.noreply.github.com>
2019-04-18 10:54:58 +0900

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,18 @@
+import os
+from ota_updater import OTAUpdater
+
+wifi_ssid = "neobby"
+wifi_password = "spdhql20!$"
+
+def download_and_install_update_if_available():
+  o = OTAUpdater('https://github.com/rnulds/Test_OTAuPy')
+  o.autoUpdate(wifi_ssid, wifi_password)
+
+def start():
+  download_and_install_update_if_available()
+
+if __name__ == '__main__':
+  start()
+
+
+
